9f9db9c0a8707a78461be3158b55ad6d8a1b148a
Emiliia <emily10sporik@gmail.com>
2026-05-07 11:44:47 +0300

    Add files via upload

---
+++ lesson_1/script/js.js
@@ -0,0 +1,68 @@
+// document.write("Hello World!");
+
+// console.log("Hello World");
+// console.info("Helloo World!");
+// console.error("Hellooo World!");
+// console.warn("Helloooo World!");
+
+// var MyFirstVar;
+// MyFirstVar = 10;
+// console.log(MyFirstVar);
+//
+// var MySecondVar = '23';
+// console.log(MySecondVar + MyFirstVar);
+// console.log(MySecondVar - MyFirstVar);
+//
+// var int = 10;
+// var float = 12.5;
+// var bool = true; //false
+// var str = "6";
+// var a;
+// a = null;
+// console.log(a);
+
+//let b = 10, a = b + 2;
+
+// a = 10;
+// var a;
+// console.log(a);
+
+// let a = 10;
+// a = 20;
+// console.log(a);
+
+// const Pi = 3.14;
+
+// let a = 10, b = 3;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// a = 2 + 2;
+// b = a + 10;
+
+// a = a + 2;
+// a /= 2;
+
+// a++; //інкремент суфіксна форма
+// a--; //декремент
+
+// ++a; //префіксна
+// --a;
+
+// let a = 2, b;
+// b = a++;
+// console.log(b);
+// console.log(a);
+
+// let a = '3', b = '5';
+// console.log((a - 0) + (b - 0));
+// console.log(Number(a) - Number(b));
+
+//Інтерполяція
+let name = 'Ivan', age = 18;
+console.log("Привіт" + name + "Тобі" + age + "років");
+console.log(`Привіт ${name}. Тобі ${age} років!`);
